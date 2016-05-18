1c6aae40b202e82f541b6975920a308642307408
Paweł Pieńkowski <pienkowskip@gmail.com>
2016-05-18 14:56:38 +0200

    Source model validation & tests.

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,2 +1,4 @@
 class Source < ApplicationRecord
+  validates :text_id, :name, presence: true
+  validates :text_id, format: {with: /\A[a-z0-9_]+\z/}
 end
--- test/models/source_test.rb
+++ test/models/source_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class SourceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should validate on save' do
+    source = Source.new
+
+    assert_not source.save
+
+    source.text_id = 'ala_ma_kota'
+    source.name = '   '
+    assert_not source.save
+
+    source.text_id = 'not valid'
+    source.name = 'Ala ma kota'
+    assert_not source.save
+
+    source.text_id = 'ala_ma_kota'
+    assert source.save
+  end
+
+  test 'should save JSON config field' do
+    source = Source.new text_id: 'ala_ma_kota', name: 'Ala ma kota'
+    source.config = {a: 10, b: 'ala ma kota', c: [1, 2, {d: 'ddd'}]}
+    assert source.save
+    assert_equal({'c' => [1, 2, {'d' => 'ddd'}], 'b' => 'ala ma kota', 'a' => 10}, source.config)
+    source.reload
+    assert_equal({'c' => [1, 2, {'d' => 'ddd'}], 'b' => 'ala ma kota', 'a' => 10}, source.config)
+  end
 end
